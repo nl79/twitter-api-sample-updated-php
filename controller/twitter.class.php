7a8dc2b08103ae0835f800cc1c1f61a290910f56
Nazar <nl79@njit.edu>
2014-10-26 14:20:09 -0400

    Updated Action handlers

--- controller/twitter.class.php
+++ controller/twitter.class.php
@@ -9,10 +9,10 @@ class twitter extends controller {
     
     /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
     private $_settings = array(
-        'oauth_access_token' => "2843175376-uYeSLZkV37SItLCY7NPEMR2QydSPFUZu3LbUIbJ",
-        'oauth_access_token_secret' => "KTa3H0zNIybSSfLm7sXzTfGeRoLgt1QIGRQQKDycNaLEE",
-        'consumer_key' => "MmMEHhzVlV68tslJZwTtFHNlv",
-        'consumer_secret' => "esZ4pvapwncZOxVYdXxzXMaZYI3R6yCocoCw5tdftal3KwhNE7"
+        'oauth_access_token' => "2843175376-aPRNxijTf2fley9GRmd7jl5d4Rs2e38PImJNEcb",
+        'oauth_access_token_secret' => "9yu9rim1VfWg0re8yI2SmF3mOJhrHGNguhn7r8mLyf3x2",
+        'consumer_key' => "Q62cPZLXNFBZ0kF0UuGRy8T1s",
+        'consumer_secret' => "gidZTOAjCWmZwgQgfFUnrkNMddS068148Q4cXerAmbIN4hd3aU"
     );
     
     
@@ -27,26 +27,7 @@ class twitter extends controller {
         */
         
     }
-    
-    protected function blockAction() {
-    
-        /** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
-        $url = 'https://api.twitter.com/1.1/blocks/create.json';
-        $requestMethod = 'POST';
-        
-        /** POST fields required by the URL above. See relevant docs as above **/
-        $postfields = array(
-            'screen_name' => 'usernameToBlock', 
-            'skip_status' => '1'
-        );
-        
-        /** Perform a POST request and echo the response **/
-        $twitter = new TwitterAPIExchange($settings);
-        echo $twitter->buildOauth($url, $requestMethod)
-                     ->setPostfields($postfields)
-                     ->performRequest();
-    }
-    
+      
     protected function tweetsAction() {
         echo('tweetAction'); 
         
@@ -55,16 +36,13 @@ class twitter extends controller {
         $getfield = '?screen_name=pamosn';
         
         $twitter = new \library\TwitterAPIExchange($this->_settings);
-        
-        $result = $twitter->setGetfield($getfield)
+       
+        $result = json_decode($twitter->setGetfield($getfield)
                      ->buildOauth($url, $requestMethod)
-                     ->performRequest(true);
-        
+                     ->performRequest(true), true); 
         echo('<pre>');
         var_dump($result);
         echo('</pre>'); 
-        
-                     
         
     }
     
@@ -72,7 +50,7 @@ class twitter extends controller {
         
         $url = 'https://api.twitter.com/1.1/users/show.json';
         $requestMethod = 'GET';
-        $getfield = '?screen_name=nl79';
+        $getfield = '?screen_name=pamosn';
         
         $twitter = new \library\TwitterAPIExchange($this->_settings);
         
@@ -80,26 +58,72 @@ class twitter extends controller {
                      ->buildOauth($url, $requestMethod)
                      ->performRequest();
         
+        $result = json_decode($twitter->setGetfield($getfield)
+                     ->buildOauth($url, $requestMethod)
+                     ->performRequest(true), true); 
         echo('<pre>');
         var_dump($result);
-        echo('</pre>'); 
+        echo('</pre>');  
     }
     
     protected function tweetAction() {
         
+        #extract the status from the request array.
+        $status = isset($_REQUEST['status']) && is_string($_REQUEST['status']) &&
+                    !empty($_REQUEST['status']); 
+        
+         /** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
+        $url = 'https://api.twitter.com/1.1/statuses/update.json';
+        $requestMethod = 'POST';
+        
+        /** POST fields required by the URL above. See relevant docs as above **/
+        $postfields = array('status'=> $status); 
+        
+        /** Perform a POST request and get the response **/
+        $twitter = new \library\TwitterAPIExchange($this->_settings);
+       
+        $result = json_decode($twitter->setPostfields($postfields)
+                     ->buildOauth($url, $requestMethod)
+                     ->performRequest(true), true); 
+        echo('<pre>');
+        var_dump($result);
+        echo('</pre>');
     }
     
     protected function timelineAction() {
+        echo('timelineAction'); 
         
+        $url = 'https://api.twitter.com/1.1/statuses/home_timeline.json';
+        $requestMethod = 'GET';
+        $getfield = '?screen_name=pamosn';
+        
+        $twitter = new \library\TwitterAPIExchange($this->_settings);
+       
+        $result = json_decode($twitter->setGetfield($getfield)
+                     ->buildOauth($url, $requestMethod)
+                     ->performRequest(true), true); 
+        echo('<pre>');
+        var_dump($result);
+        echo('</pre>');
     }
-    
-    
     
     protected function followersAction() {
-          
+         echo('followersAction'); 
+        
+        $url = 'https://api.twitter.com/1.1/followers/list.json';
+        $requestMethod = 'GET';
+        $getfield = '?screen_name=pamosn';
+        
+        $twitter = new \library\TwitterAPIExchange($this->_settings);
+       
+        $result = json_decode($twitter->setGetfield($getfield)
+                     ->buildOauth($url, $requestMethod)
+                     ->performRequest(true), true);
+        
+        echo('<pre>');
+        var_dump($result);
+        echo('</pre>'); 
     }
-    
-    
     
     protected function followersidsAction() {
         
